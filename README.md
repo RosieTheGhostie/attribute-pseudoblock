2d2a4d5d5afc62fe709a05d056276f8b7495f5e7
RosieTheGhostie <mdcartkids@gmail.com>
2025-02-11 09:52:48 -0700

    it's attribute-pseudoblock now

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# seaside-int-utils
+# attribute-pseudoblock
 
-A small set of integer utilities for [seaside](https://github.com/RosieTheGhostie/seaside).
+Apply an attribute to a section of code that can't be put in a block.
+
+_This crate's code is very slightly tweaked from Todd's answer in [this Stack Overflow thread]._
+
+[this Stack Overflow thread]: https://stackoverflow.com/questions/58602358/applying-a-rust-attribute-to-more-than-one-line
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,40 @@
-#[cfg(feature = "byte_stream")]
-pub mod byte_stream;
-#[cfg(feature = "endian")]
-pub mod endian;
-#[cfg(feature = "sign_extend")]
-pub mod sign_extend;
-
-#[cfg(feature = "byte_stream")]
-pub use byte_stream::ByteStream;
-#[cfg(feature = "endian")]
-pub use endian::Endian;
-#[cfg(feature = "sign_extend")]
-pub use sign_extend::SignExtend;
+/// Applies the same attribute to all items in the block, but doesn't compile it as an actual block.
+///
+/// Code is very slightly tweaked from Todd's answer in [this Stack Overflow thread](https://stackoverflow.com/questions/58602358/applying-a-rust-attribute-to-more-than-one-line).
+///
+/// # Examples
+///
+/// Say you wanted to lock most of a file behind a certain feature "bar". With [`pseudoblock`], you
+/// can do that without repeating the same attribute for practically every line of code.
+///
+/// ```ignore
+/// # #[macro_use] extern crate attribute_pseudoblock;
+/// pub mod foo;
+///
+/// psuedoblock! {
+///     #![cfg(feature = "bar")]
+///
+///     mod baz;
+///
+///     use baz::{bee, buzz, fizz};
+///
+///     pub fn some_api_thing(x: i32, y: i32) -> Option<i32> {
+///         // ...
+/// #       None
+///     }
+///
+///     pub fn another_api_thing(user_id: u64, password_sha256: [u8; 32]) -> bool {
+///         // ...
+/// #       false
+///     }
+/// }
+/// ```
+#[macro_export]
+macro_rules! pseudoblock {
+    { #!$attr:tt $($it:item)* } => {
+        $(
+            #$attr
+            $it
+        )*
+    };
+}
